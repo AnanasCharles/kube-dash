4e6d87125c32c6add26dca9302b229acc7b37852
AnanasCharles <ananascharles@proton.me>
2024-02-07 14:57:30 +0100

    links

--- src/App.js
+++ src/App.js
@@ -1,32 +1,30 @@
-import './App.css';
-import { SiKubernetes, SiReact, SiArgo } from 'react-icons/si';
+import "./App.css";
+import { SiKubernetes, SiReact, SiArgo } from "react-icons/si";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className='text-4xl font-bold tracking-wide'>
-          Docker-Server
-        </div>
-        <div className='flex p-2'>
+        <div className="text-4xl font-bold tracking-wide">Docker-Server</div>
+        <div className="flex p-2">
+          <a href="https://docker-server:31111" target="_blank" rel="noreferrer">
           <div>
-          <SiKubernetes size={128} className='m-2' color='#326ce5' />
-          <div className='bg-blue-600 p-2 rounded-md'>
-            K8S
+            <SiKubernetes size={128} className="m-2" color="#326ce5" />
+            <div className="bg-blue-600 p-2 rounded-md">K8S</div>
           </div>
+          </a>
+          <div className="m-4 mt-16">+</div>
+          <a href="https://docker-server:31112" target="_blank" rel="noreferrer">
+          <div>
+            <SiArgo size={128} className="m-2" color="#ed820e" />
+            <div className="bg-orange-600 p-2 rounded-md">ArgoCD</div>
           </div>
-          <div className='m-4 mt-16'>+</div>
+          </a>
 
+          <div className="m-4 mt-16">+</div>
           <div>
-          <SiArgo size={128} className='m-2' color='#ed820e'/>
-          <div className='bg-orange-600 p-2 rounded-md'>
-            ArgoCD
-          </div>
-          </div>
-          <div className='m-4 mt-16'>+</div>
-          <div>
-          <SiReact size={128} className='m-2' color='#04d8f9'/>
-          <div className='bg-blue-500 p-2 rounded-md'>React</div>
+            <SiReact size={128} className="m-2" color="#04d8f9" />
+            <div className="bg-blue-500 p-2 rounded-md">React</div>
           </div>
         </div>
         {/* <div className='flex'>
